b67eb0bec85562df6c81b01ec71d291e45d63c47
MartynasVCS <martynasvcs@gmail.com>
2023-02-08 21:05:05 +0200

    Add solution to task 1 subtask 1

---
+++ Uzduotis01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uzduotis01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1 punktas
+             * 
+             * Parašykite programą, 
+             * kuri išvestų žodį “Labas” 
+             * ir jūsų vardą toje pačioje eilutėje.
+             */
+            
+            Console.WriteLine("Labas Martynai");
+        }
+    }
+}
