86a63ad3ad72ecd6e432bd0600279a52bc5b1af6
MartynasVCS <martynasvcs@gmail.com>
2023-02-13 19:35:15 +0200

    Add demo code for topic about switch statement

---
+++ SwitchDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SwitchDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string reiksme = "Katė";
+
+            switch (reiksme)
+            {
+                case "šuo":
+                    Console.WriteLine("Neteisingas");
+                    break;
+                case "Šuo":
+                    Console.WriteLine("Teisingas!");
+                    break;
+                // default atvejis nėra privalomas
+                default:
+                    Console.WriteLine("Visi kiti atvejai");
+                    break;
+            }
+
+            // Analogiškas atvejis su if
+            if (reiksme == "šuo")
+            {
+                Console.WriteLine("Neteisingas");
+            }
+            else if (reiksme == "šuo")
+            {
+                Console.WriteLine("Teisingas!");
+            }
+            else
+            {
+                Console.WriteLine("Visi kiti atvejai");
+            }
+        }
+    }
+}
