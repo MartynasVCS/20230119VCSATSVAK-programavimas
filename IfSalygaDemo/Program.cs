5169e731670980e954c698b75a8eacaaac8e9aac
MartynasVCS <martynasvcs@gmail.com>
2023-02-13 18:01:41 +0200

    Add demo code for topics about Console input and output

---
+++ IfSalygaDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace IfSalygaDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            int skaicius = 3;
+
+            if (skaicius > 4)
+            {
+                Console.WriteLine("Taip");
+            }
+            Console.WriteLine("Tolimesnis kodas");
+
+            // Jei tenkinant if sąlyga norime atlikti tik vieną veiksmą tai galime rašyti taip:
+            if (5 > 4) Console.WriteLine("Valio!!!");
+            // Arba šitaip
+            if (5 > 4) 
+                Console.WriteLine("Valio!!!");
+            // Bet geriau taip nedaryti ;)
+
+            string tekstas = "Šuo";
+            if (tekstas == "Šuo")
+            {
+                Console.WriteLine("Šuo!!!");
+            }
+
+            Console.Write("Įveskite skaičių: ");
+            int ivestasSkaicius = Convert.ToInt32(Console.ReadLine());
+            if (ivestasSkaicius % 2 == 0)
+            {
+                Console.WriteLine($"Įvestas skaičius: {ivestasSkaicius}");
+                Console.WriteLine($"Skaičius penktuoju laipsniu: {Math.Pow(ivestasSkaicius, 5)}");
+            }
+
+            Console.WriteLine("Pabaiga");
+
+            if (true == true) { }
+            else if (true) { }
+            else if (true) { }
+            else if (true) { }
+            else if (true) { }
+            else if (true) { }
+            else if (true) { }
+            else { }
+        }
+    }
+}
---
+++ IvedimasDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace IvedimasDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.Write("Įveskite teksto eilutę: ");
+            string ivedimas = Console.ReadLine();
+            Console.WriteLine($"Jūs įvedėte: {ivedimas}.");
+            Console.WriteLine();
+
+            // Galime nekurti kintamojo, bet taip yra sunkiau skaityti.
+            Console.WriteLine("Įveskite teksto eilutę:");
+            Console.WriteLine($"Jūs įvedėte: {Console.ReadLine()}.");
+            Console.WriteLine();
+
+            Console.Write("Įveskite pirmą skaičių: ");
+            int skaicius1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Įveskite antrą skaičių: ");
+            int skaicius2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Suma: {skaicius1 + skaicius2}");
+            string penki = Convert.ToString(5);
+            Console.WriteLine();
+
+            Console.Write("Įveskite true arba false: ");
+            bool trueFalse = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine($"Įvedėte: {trueFalse}");
+            Console.WriteLine();
+
+            Console.Write("Įveskite simbolį: ");
+            char simbolis = Convert.ToChar(Console.Read());
+            Console.ReadLine(); // Šios eilutės reikia jeigu darome papildomą įvedimą po Console.Read()
+            Console.WriteLine($"Įvedėte: {simbolis}");
+            Console.Write("Įveskite dar ką nors: ");
+            string papildomasIvedimas = Console.ReadLine();
+            Console.WriteLine($"Įvedėte: {papildomasIvedimas}");
+            Console.WriteLine();
+
+            Console.Write("Įveskite savo vardą: ");
+            string vardas = Console.ReadLine();
+            Console.Write("Įveskite savo amžių: ");
+            int amzius = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Jūsų vardas yra {vardas} o amžius {amzius}");
+            Console.WriteLine();
+
+            Console.WriteLine(Math.Min(5, 10));
+            Console.WriteLine(Math.Max(5, 10));
+            Console.WriteLine(Math.PI);
+            Console.WriteLine(Math.Sqrt(5));
+            Console.WriteLine(Math.Pow(5, 1/2));
+            Console.WriteLine(Math.Pow(5, 2));
+        }
+    }
+}
