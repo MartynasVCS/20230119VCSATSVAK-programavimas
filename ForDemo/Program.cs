242dc08d289596836e12fb6788f4e3fe113cbac0
MartynasVCS <martynasvcs@gmail.com>
2023-02-13 20:09:55 +0200

    Add demo code for topic about For loop

---
+++ ForDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ForDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                // Čia rašom veiksmus norim atlikti
+                Console.WriteLine("Labas");
+            }
+
+            for (int i = 0; i < 25; i += 5)
+            {
+                // Čia rašom veiksmus norim atlikti
+                Console.WriteLine("Ate");
+            }
+
+            for (double i = 1000000; i > 5; i = Math.Sqrt(i))
+            {
+                // Čia rašom veiksmus norim atlikti
+                Console.WriteLine(i);
+            }
+
+            // amžinas ciklas
+            for (int i = 0; i >= 0; i++)
+            {
+                // Čia rašom veiksmus norim atlikti
+                Console.WriteLine("Kada pabaiga?");
+            }
+
+            /* 
+             * Rasti visų skaičių, 
+             * žemesnių už 1000 ir 
+             * didesnių už 0 
+             * bei kurie dalinasi iš 
+             * 3 arba 5
+             * sumą.
+             */
+
+            int suma = 0;
+
+            for (int i = 0; i < 1000; i++ )
+            {
+                if ( i % 3 == 0 || i % 5 == 0 )
+                {
+                    suma += i;
+                }
+                
+            }
+            
+            Console.WriteLine(suma);
+        }
+    }
+}
