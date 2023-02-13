ba764b9e88bf984e02f91ca1c15ffd83058b3eed
MartynasVCS <martynasvcs@gmail.com>
2023-02-13 20:32:17 +0200

    Add demo code for topic about While loop

---
+++ WhileDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace WhileDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            // For ir While palyginimas
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            int skaicius = 0;
+            while (skaicius <= 10)
+            {
+                Console.WriteLine(skaicius);
+                skaicius++;
+            }
+
+            // Ne visiškai geras pavyzdys, bet pusiau veikiantis pavyzdys
+            // parodantis kaip apsisaugoti nuo netinkamo vartotojo įvedimo
+            int ivedimas = 0;
+            while (ivedimas <= 0)
+            {
+                try
+                {
+                    Console.Write("Įveskite naturalųjį skaičių: ");
+                    ivedimas = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"Įvedėte skaičių {ivedimas}");
+                } 
+                catch
+                {
+                    Console.WriteLine("Įvedėte netinkamą reikšmę!");
+                }
+            }
+        }
+    }
+}
